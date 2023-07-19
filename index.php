abbb0bf63164530ac3938a35bd00584e20b8212c
YoussefGomaa <youssef123gomaa@gmail.com>
2023-07-19 19:30:05 +0300

    Medical System

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php require '../config.php';  ?>
+<?php require BLA.'inc/header.php';  ?>
+
+
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">All  Services</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="<?php echo BLA.'services/' ?>" class="btn btn-primary"></a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">All Cities</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="<?php echo BLA.'cities/' ?>" class="btn btn-primary"></a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-sm-6 mt-5">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">All Orders</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="<?php echo BLA.'orders/' ?>" class="btn btn-primary"></a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-sm-6 mt-5">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">All Orders This Day</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="<?php echo BLA.'orders/' ?>" class="btn btn-primary"></a>
+      </div>
+    </div>
+  </div>
+
+
+
+<?php require BLA.'/inc/footer.php';  ?>
+
+
+
+   
+
